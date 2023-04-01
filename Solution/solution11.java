3c7a7ab77bf43c0bca644c342f0014436835ab16
21bca49 <96609121+21bca49@users.noreply.github.com>
2023-04-01 15:10:19 +0530

    Create solution11.java

---
+++ Solution/solution11.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.applet.*;
+
+public class solution11 extends Applet {
+	
+    public void paint(Graphics g) {
+
+        g.setColor(Color.red);
+        g.fillRect(200,200,400,400);
+
+        g.setColor(Color.yellow);
+        Font font = new Font("Times New Roman", Font.PLAIN, 50);
+        g.setFont(font);
+        FontMetrics metrics = g.getFontMetrics(font);
+        int x = (200 - metrics.stringWidth("My College")) / 2;
+        int y = ((200 - metrics.getHeight()) / 2) + metrics.getAscent();
+        g.drawString("VTCBCSR", 300+x, 300+y);
+		
+    }
+}
